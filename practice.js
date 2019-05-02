aa01e6121f53bb57deb0d78fef3be991e13dd327
johncook <jonnymcook1@gmail.com>
2019-05-01 21:30:34 -0500

    initial commit

--- practice.js
+++ practice.js
@@ -28,6 +28,9 @@
 */
 
 // Code Here 
+function first( arr, cb) {
+  cb(arr[0])
+}
 
 // Do not edit the code below.
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
@@ -48,7 +51,10 @@ first(names, function(firstName){
 */
 
 //Code Here
+function last(arr, cb) {
+  cb(arr[arr.length-1])
 
+}
 // Do not edit the code below.
 last(names, function(lastName){
   console.log('The last name in names is ' + lastName);
@@ -66,8 +72,11 @@ last(names, function(lastName){
 */
 
 //Code Here
-
-// Do not edit the code below.
+function multiply(num1, num2, cb) {
+  cb(num1*num2)
+}
+  
+  // Do not edit the code below.
 multiply(4, 3, function(answer){
   console.log('The answer is ' + answer); //should console.log 12
 });
@@ -85,13 +94,22 @@ multiply(4, 3, function(answer){
 */
 
 //Code Here 
+function contains(arr, str, cb) { 
+  if(arr.includes(str)) {
+    cb(true)
+  }else{
+    cb(false)
+  }
+}
+
+
 
 // Do not edit the code below.
 contains(names, 'Colt', function(result){
   if(result === true){
-    console.log('Colt is in the array');
+    console.log('Colt is in the array')
   } else {
-    console.log('Colt is not in the array');
+    console.log('Colt is not in the array')
   }
 });
 // Do not edit the code above.
@@ -107,6 +125,16 @@ contains(names, 'Colt', function(result){
 
 //Code Here
 
+function uniq(names, cb) {
+  for (var i = 0; i < names.length; i++) {
+    for (var j = i + 1; j < names.length; j++) {
+      if (names[i] === names[j]) {
+        names.splice(j, 1)
+      }
+    }
+  }
+  cb(names)
+}
 // Do not edit the code below.
 uniq(names, function(uniqArr){
   console.log('The new names array with all the duplicate items removed is ', uniqArr);
@@ -123,6 +151,12 @@ uniq(names, function(uniqArr){
 */
 
 //Code Here 
+function each(names, cb) {   
+  for (var i = 0; i < names.length; i++) {
+    cb(names[i], i)
+  } 
+  return names
+}
 
 // Do not edit the code below.
 each(names, function(item, indice){
@@ -141,6 +175,14 @@ each(names, function(item, indice){
 
 // Code here
 
+function getUserById (users, id, cb){
+  console.log(users[0].id)
+  for(let i = 0; i < users.length; i++){
+    if(users[i].id === id){
+      return cb(users[i])
+    }
+  }
+}
 // Do not edit the code below.
 var users = [
   {
